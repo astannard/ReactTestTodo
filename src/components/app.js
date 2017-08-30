cb95c875afc1c36ac7f5cc188f73016af8cb0754
andy <andy@inniaccounts.co.uk>
2017-08-30 14:35:04 +0100

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap';
+class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            text: ''
+        };
+    }
+
+    render(){
+
+        return(
+            <div>
+                    <h2>Note to self</h2>
+                    <Form inline>
+                        <FormControl onChange={event => {
+                            this.setState({text:event.target.value})
+                            }}/>
+                            {' '}
+                        <Button onClick={() => console.log(this.state)}>Submit</Button>
+                    </Form>
+                </div>
+        )
+    }
+}
+
+export default App;
